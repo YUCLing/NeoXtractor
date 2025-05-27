5867a3a5857bf48aea282559c3f6595c16ddf474
LuoRain <luotianyi@luotianyi.me>
2025-05-27 20:29:13 +0800

    chore: better build_shaders.py outputs

--- tools/build_shaders.py
+++ tools/build_shaders.py
@@ -2,6 +2,7 @@
 
 import os
 import subprocess
+import textwrap
 
 project_dir = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
 
@@ -9,7 +10,13 @@ for filename in os.listdir(os.path.join(project_dir, "shaders")):
     if filename.endswith(".vert") or filename.endswith(".frag"):
         shader_path = os.path.join(project_dir, "shaders", filename)
         output_path = os.path.join(project_dir, "data", "shaders", filename + ".qsb")
-        subprocess.run(
-            ["pyside6-qsb", "--qt6", shader_path, "-o", output_path],
-            check=True,
-        )
+        result = subprocess.run(
+                ["pyside6-qsb", "--qt6", shader_path, "-o", output_path],
+                capture_output=True,
+                check=False
+            )
+        if result.returncode == 0:
+            print(f"> Built shader {filename} (to {filename + ".qsb"}) successfully.")
+        else:
+            print(f"> Error building shader {filename}")
+            print(textwrap.indent(result.stderr.decode(), "-> "))
