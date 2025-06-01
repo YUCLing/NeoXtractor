ce8b530d810992bc294d65f3ecc25302adff11bb
LuoRain <luotianyi@luotianyi.me>
2025-06-01 17:18:34 +0800

    add: MSAA selector

--- _temporary_tools_/run_mesh_viewer.py
+++ _temporary_tools_/run_mesh_viewer.py
@@ -28,14 +28,27 @@ if __name__ == "__main__":
 
     open_button.clicked.connect(open_mesh_file)
 
+    msaa_combobox = QtWidgets.QComboBox()
+    layout.addWidget(msaa_combobox)
+
     viewer = MeshViewer()
-    viewer.render_widget.setApi(QtWidgets.QRhiWidget.Api.Vulkan)
+    viewer.render_widget.setApi(QtWidgets.QRhiWidget.Api.OpenGL)
 
     layout.addWidget(viewer)
 
     wnd.setWindowTitle(f"Mesh Viewer - {viewer.render_widget.api().name}")
     wnd.resize(800, 600)
     wnd.show()
+
+    for sample_count in viewer.render_widget.rhi().supportedSampleCounts():
+        if sample_count == 1:
+            msaa_combobox.addItem("No MSAA", 1)
+        else:
+            msaa_combobox.addItem(f"{sample_count}x MSAA", sample_count)
+    msaa_combobox.setCurrentIndex(0)
+    def set_msaa(value):
+        viewer.render_widget.setSampleCount(value)
+    msaa_combobox.currentIndexChanged.connect(lambda idx: set_msaa(msaa_combobox.itemData(idx)))
 
     print(viewer.render_widget.api())
     sys.exit(app.exec())
